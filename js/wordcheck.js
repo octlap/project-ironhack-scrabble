80a0355d76bc79dbb6c172cbca205a94de763c07
Octave Lapeyronie <octave.lapeyronie@gmail.com>
2017-07-17 15:11:29 +0200

    Fixed https issue

--- js/wordcheck.js
+++ js/wordcheck.js
@@ -1,7 +1,7 @@
 function checkWord(word) {
 
   var result = $.ajax({
-    url: "http://www.wordgamedictionary.com/api/v1/references/scrabble/" + word + "?key=1.1718115275527805e30",
+    url: "https://www.wordgamedictionary.com/api/v1/references/scrabble/" + word + "?key=1.1718115275527805e30",
     method: "GET",
     async: false,
     success: function (response) {
